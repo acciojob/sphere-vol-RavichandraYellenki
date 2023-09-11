07b45f23a650b11f3d77f73c4063e60ad6102b29
RavichandraYellenki <134284687+RavichandraYellenki@users.noreply.github.com>
2023-09-11 23:33:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+  let inputRadius = document.getElementById("radius");
+	let r = parseFloat( inputRadius.value);
+	let outputElement = document.getElementById("volume");
+
+	let outputVolume = (4/3)*(Math.PI)* Math.pow(r, 3);
+	outputElement.value=outputVolume;
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function () {
+  document.getElementById('MyForm').onsubmit = function (event) {
+    event.preventDefault();
+    volume_sphere();
+  };
+};
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere();
